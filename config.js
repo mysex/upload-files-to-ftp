914cc81edcafbcfe767c6bcf3c57995f06e16953
xtl <1085075895@qq.com>
2018-12-25 00:27:40 +0800

    'toolcreate'

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports={
+    serve:{
+        host: "192.168.1.12",
+        port: 21,
+        user: "ftproot",
+        password: "root"
+    },
+    localDir:"docs",
+    serveDir:"/www"
+}
---
+++ index.js
@@ -0,0 +1,72 @@
+let fs = require('fs');
+
+let path = require('path');
+let Ftp = require("ftp");
+let config = require("./config");
+let travel = require("./travel")
+
+let serveDir = config.serveDir;
+let localDir = config.localDir;
+
+let client = new Ftp();
+
+
+client.connect(config.serve);
+client.on('ready', function () {
+    travel( localDir, function (pathname, next) {
+        let dirName = path.dirname(pathname).replace(localDir, serveDir);
+        let serveFilePath = path.join(dirName, path.basename(pathname));
+        client.get(dirName, function (err, list) {
+            if (err) {
+                client.mkdir(dirName, true, function (err) {
+                    client.put(pathname, serveFilePath, function (err) {
+                        if (err) {
+                            console.log("本地路径", pathname)
+                            console.log("服务器", serveFilePath)
+                            console.log("上传失败");
+                            console.log(err)
+                        } else {
+                            console.log("本地路径", pathname)
+                            console.log("服务器", serveFilePath)
+                            console.log("上传成功");
+                            next()
+                        }
+                    })
+                })
+            } else {
+                client.put(pathname, serveFilePath, function (err) {
+                    if (err) {
+                        console.log("本地路径", pathname)
+                        console.log("服务器", serveFilePath)
+                        console.log("上传失败");
+                        console.log(err)
+                    } else {
+                        console.log("本地路径", pathname)
+                        console.log("服务器", serveFilePath)
+                        console.log("上传成功");
+                        next()
+                    }
+                })
+            }
+        })
+    }, function () {
+        console.log("文件上传完成");
+        client.end();
+    })
+})
+
+client.on("error", function (err) {
+    console.log("链接失败");
+    console.log(err)
+})
+
+client.on('greeting', function (msg) {
+    console.log(msg)
+})
+
+// client.on('close',function (hadErr) {
+//     console.log(hadErr)
+// })
+client.on("end", function () {
+    console.log('上传完成！！！！')
+})
